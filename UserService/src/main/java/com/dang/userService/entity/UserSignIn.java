fbdd1f041483afe5654464982ecdec2a595b3d3f
dangvuminh <dangvuminh98@gmail.com>
2021-08-29 21:55:41 +0700

    get question list by category  -api sign in - api

---
+++ UserService/src/main/java/com/dang/userService/entity/UserSignIn.java
@@ -0,0 +1,32 @@
+package com.dang.userService.entity;
+
+import javax.validation.constraints.NotNull;
+
+public class UserSignIn {
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    public UserSignIn(@NotNull String username, @NotNull String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
