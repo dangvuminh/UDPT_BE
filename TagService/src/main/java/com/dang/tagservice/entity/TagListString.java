016fd034fdc691edf48f8a5bfc7a458ab7655a54
dangvuminh <dangvuminh98@gmail.com>
2021-09-03 10:51:52 +0700

    get user list -api user add tags when creating refactor  get legal and illegal questions api

---
+++ TagService/src/main/java/com/dang/tagservice/entity/TagListString.java
@@ -0,0 +1,20 @@
+package com.dang.tagservice.entity;
+
+public class TagListString {
+    private String tagList ;
+
+    public TagListString() {
+    }
+
+    public TagListString(String tagList) {
+        this.tagList = tagList;
+    }
+
+    public String getTagList() {
+        return tagList;
+    }
+
+    public void setTagList(String tagList) {
+        this.tagList = tagList;
+    }
+}
