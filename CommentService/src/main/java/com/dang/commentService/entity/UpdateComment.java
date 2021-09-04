8707d065eceb59d3d17559973bdc23b72ab8f2ab
dangvuminh <dangvuminh98@gmail.com>
2021-09-04 17:30:34 +0700

    comment update - api profile update - api

---
+++ CommentService/src/main/java/com/dang/commentService/entity/UpdateComment.java
@@ -0,0 +1,49 @@
+package com.dang.commentService.entity;
+
+import com.sun.istack.NotNull;
+
+public class UpdateComment {
+    @NotNull
+    private String commentContent;
+
+    @NotNull
+    private Integer forumId;
+
+    @NotNull
+    private String userId;
+
+    @NotNull
+    private Integer commentId;
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
+        this.commentContent = commentContent;
+    }
+
+    public Integer getForumId() {
+        return forumId;
+    }
+
+    public void setForumId(Integer forumId) {
+        this.forumId = forumId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Integer commentId) {
+        this.commentId = commentId;
+    }
+}
---
+++ UserService/src/main/java/com/dang/userService/entity/UpdateProfile.java
@@ -0,0 +1,60 @@
+package com.dang.userService.entity;
+
+import javax.validation.constraints.NotNull;
+
+public class UpdateProfile {
+    @NotNull
+    private String userId;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String userName;
+
+    @NotNull
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
